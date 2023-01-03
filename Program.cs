c8d204e330f95f65fb4ab9935c8861f5b0c64d0c
icmosoftware <ozendeniz@gmail.com>
2023-01-03 19:12:15 +0300

    ConsoleProgram Update1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleProgram;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        int x=20;
+
+        string xString = x.ToString();
+
+        Console.WriteLine("X'in string değeri: " + xString);
+
+        string s="100";
+        x = Convert.ToInt32(s);
+
+        Console.WriteLine ("X'in yeni değeri: " + x);
+
+    }
+}
